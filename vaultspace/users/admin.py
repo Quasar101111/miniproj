e48ad1eae0be61cb6ca76620a4b9dfb88712e0e5
Quasar101111 <165417250+Quasar101111@users.noreply.github.com>
2024-10-26 11:10:04 +0530

    Delete vaultspace directory

--- vaultspace/users/admin.py
+++
@@ -1,8 +0,0 @@
-# users/admin.py
-
-from django.contrib import admin
-from .models import Profile,Lessor,Tenant
-
-admin.site.register(Profile)
-admin.site.register(Lessor)
-admin.site.register(Tenant)
