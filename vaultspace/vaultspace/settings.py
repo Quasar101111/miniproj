e714ee96dadae084efc7eb69cb2966a1f41971d9
Quasar101111 <165417250+Quasar101111@users.noreply.github.com>
2024-10-27 23:23:42 +0530

    Update settings.py

--- vaultspace/vaultspace/settings.py
+++ vaultspace/vaultspace/settings.py
@@ -120,7 +120,7 @@ if not DEBUG:    # Tell Django to copy static assets into a path called `staticf
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
