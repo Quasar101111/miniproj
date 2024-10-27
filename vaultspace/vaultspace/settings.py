be6d6c2a17dd3c6f807a53f0ef7e9fa6a583ee8d
Quasar101111 <165417250+Quasar101111@users.noreply.github.com>
2024-10-27 23:19:41 +0530

    Update settings.py

--- vaultspace/vaultspace/settings.py
+++ vaultspace/vaultspace/settings.py
@@ -116,6 +116,11 @@ ASGI_APPLICATION = 'users.asgi.application'
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
